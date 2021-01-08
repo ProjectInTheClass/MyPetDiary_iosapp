1daaa59866c7d02221b94dba8c814a5f46cf031d
Eun Young <eyeyl0811@naver.com>
2021-01-08 21:16:29 +0900

    update withdrawal

---
+++ MyPetDiary/DogWalker_iosapp/ViewController/WithdrawalViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WithdrawalViewController.swift
+//  DogWalker_iosapp
+//
+//  Created by EunYoung on 2021/01/08.
+//
+
+import UIKit
+
+class WithdrawalViewController: UIViewController {
+
+    @IBAction func deleteUser(_ sender: Any) {
+    }
+    @IBAction func keepUser(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
