a9fd7a2b99fe1cee92034c1fde1afb4c7f0b362a
Eun Young <eyeyl0811@naver.com>
2021-01-08 20:49:03 +0900

    create SettingViewController.swift

---
+++ MyPetDiary/DogWalker_iosapp/ViewController/SettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingViewController.swift
+//  DogWalker_iosapp
+//
+//  Created by EunYoung on 2021/01/08.
+//
+
+import UIKit
+
+class SettingViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
