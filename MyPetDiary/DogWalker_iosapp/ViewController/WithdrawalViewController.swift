b7a9cce9ff625ebb9cf915ba6121e5bdd1269cee
Eun Young <eyeyl0811@naver.com>
2021-01-12 17:18:35 +0900

    back to SignUp

--- MyPetDiary/DogWalker_iosapp/ViewController/WithdrawalViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/WithdrawalViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class WithdrawalViewController: UIViewController {
+    
+    var window: UIWindow?
 
     @IBAction func deleteUser(_ sender: Any) {
         func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if segue.identifier == "backToSetting" {
                 let vc = segue.destination as! SettingViewController
             }
+            
         }
         // create the alert
         let alert = UIAlertController(title: "탈퇴 완료", message: "기존 데이터가 삭제되었습니다", preferredStyle: UIAlertController.Style.alert)
@@ -25,7 +28,7 @@ class WithdrawalViewController: UIViewController {
         
         // add an action (button)
         alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler:
-                                        { _ in self.goBackToSetting()}
+                                        { _ in self.goBackToSignUp()}
         ))
 
         // show the alert
@@ -42,8 +45,20 @@ class WithdrawalViewController: UIViewController {
         }
     }
     
-    func goBackToSetting() {
-        performSegue(withIdentifier: "backToSetting", sender: self)
+    func goBackToSignUp() {
+        let window = UIApplication.shared.keyWindow!
+        let frame = window.rootViewController!.view.frame
+
+        let controller = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "SignUpView")
+
+        controller.view.frame = frame
+
+        UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: {
+            window.rootViewController = controller
+        }, completion: { completed in
+            // maybe do something here
+        })
     }
     
     override func viewDidLoad() {
