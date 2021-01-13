8f0aedf3c9f34977f7decff9cfa8809ac4e43def
Eun Young <eyeyl0811@naver.com>
2021-01-13 20:36:01 +0900

    Create Poster Session SandBox

--- SandBox/PosterSession/PosterSession/PosterSession/ViewController.swift
+++ SandBox/PosterSession/PosterSession/PosterSession/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myImage2: UIImageView!
+    @IBOutlet weak var feedImage1: UIImageView!
+    @IBOutlet weak var feedImage2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        myImage.layer.cornerRadius = 20
+        myImage.clipsToBounds = true
+        myImage.layer.borderWidth = 2
+        myImage.layer.borderColor = UIColor.lightGray.cgColor
+        myImage2.layer.cornerRadius = 20
+        myImage2.clipsToBounds = true
+        myImage2.layer.borderWidth = 2
+        myImage2.layer.borderColor = UIColor.lightGray.cgColor
+        
+        feedImage1.clipsToBounds = true
+        feedImage1.layer.borderWidth = 1
+        feedImage1.layer.borderColor = UIColor.lightGray.cgColor
+        
+        feedImage2.clipsToBounds = true
+        feedImage2.layer.borderWidth = 1
+        feedImage2.layer.borderColor = UIColor.lightGray.cgColor
+        
+//        let view = UIView()
+//        view.backgroundColor = .white
+//        
+//        let dogImage = UIImage(named: "dog.jpg")
+//        
+//        let myImageView: UIImageView = UIImageView()
+//        myImageView.contentMode = UIView.ContentMode.scaleAspectFill
+//        myImageView.frame.size.width = 300
+//        myImageView.frame.size.height = 300
+//        myImageView.center = self.view.center
+//        
+//        myImageView.image = dogImage
+//        
+//        myImageView.layer.cornerRadius = 100
+//        myImageView.clipsToBounds = true
+//        myImageView.layer.borderWidth = 10
+//        myImageView.layer.borderColor = UIColor.lightGray.cgColor
+//        
+//        view.addSubview(myImageView)
+//        
+//        self.view = view
     }
-
-
 }
 
