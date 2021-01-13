579755311cd3326c167e845a0c148601f6a3550f
howard7221 <66044780+howard7221@users.noreply.github.com>
2021-01-14 01:38:02 +0900

    add NewsFeed

--- MyPetDiary/DogWalker_iosapp/Model/Post.swift
+++ MyPetDiary/DogWalker_iosapp/Model/Post.swift
@@ -25,14 +25,24 @@ struct Post{
         
         let Howard = FeedUser(username: "Howard", profileImage: UIImage(named: "hana"))
         
-        let Mary = FeedUser(username: "Mary", profileImage: UIImage(named: "mary"))
+        let Mary = FeedUser(username: "마리언니", profileImage: UIImage(named: "mary"))
         
-        let post1 = Post(createdBy: Howard, image: UIImage(named: "1"))
-        let post2 = Post(createdBy: Howard, image: UIImage(named: "2"))
+        let Dongguk = FeedUser(username: "동국이", profileImage: UIImage(named: "ddog"))
+        
+        let Coders = FeedUser(username: "Coders", profileImage: UIImage(named: "puppy"))
+       
+        let Kim = FeedUser(username: "Kim", profileImage: UIImage(named: "3"))
+
+        let dream = FeedUser(username: "드림이", profileImage: UIImage(named: "hana"))
+
+        
+    
+        let post1 = Post(createdBy: Dongguk, image: UIImage(named: "1"))
+        let post2 = Post(createdBy: Coders, image: UIImage(named: "2"))
         let post3 = Post(createdBy: Howard, image: UIImage(named: "3"))
        
-        let post4 = Post(createdBy: Mary, image: UIImage(named: "4"))
-        let post5 = Post(createdBy: Mary, image: UIImage(named: "5"))
+        let post4 = Post(createdBy: Kim, image: UIImage(named: "4"))
+        let post5 = Post(createdBy: dream, image: UIImage(named: "5"))
         let post6 = Post(createdBy: Mary, image: UIImage(named: "6"))
         
         posts.append(post1)
