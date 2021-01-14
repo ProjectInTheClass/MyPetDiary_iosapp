bfad32df5ed10f78eb6c857e36e3d70e7a510996
Eun Young <eyeyl0811@naver.com>
2021-01-14 21:12:53 +0900

    database insert user

--- MyPetDiary/DogWalker_iosapp/ViewController/ViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/ViewController.swift
@@ -7,11 +7,8 @@
 
 import UIKit
 import Firebase
-import FirebaseDatabase
 
 class ViewController: UIViewController {
-    
-    var ref: DatabaseReference! = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
