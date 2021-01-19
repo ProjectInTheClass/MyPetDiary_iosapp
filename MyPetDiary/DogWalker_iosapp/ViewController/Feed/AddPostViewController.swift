e4d12755bde6b07592a803ba7c1913b7996cac3e
Eun Young <eyeyl0811@naver.com>
2021-01-19 17:55:00 +0900

    Update AddPostViewController.swift

--- MyPetDiary/DogWalker_iosapp/ViewController/Feed/AddPostViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/Feed/AddPostViewController.swift
@@ -42,7 +42,7 @@ class AddPostViewController: UIViewController, UITextFieldDelegate {
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         var current_date_string = formatter.string(from: Date())
         
-        if textField.text == nil { // textField에 아무것도 안썼을 경우
+        if textField.text == "" { // textField에 아무것도 안썼을 경우
             // create the alert
             let alert = UIAlertController(title: "내용이 비어있음", message: "글을 작성해주세요", preferredStyle: UIAlertController.Style.alert)
             // add an action (button)
