4e51171f7537c2972f5e0f3514bcdee621474e77
jiyeon <jjy1000@dgu.ac.kr>
2021-01-21 03:07:08 +0900

    my page collectionview

--- MyPetDiary/DogWalker_iosapp/ViewController/MyPage/MyPageViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/MyPage/MyPageViewController.swift
@@ -2,27 +2,24 @@
 //  MyPageViewController.swift
 //  DogWalker_iosapp
 //
-//  Created by EunYoung on 2021/01/08.
+//  Created by 정지연 on 2021/01/21.
 //
 
 import UIKit
 import Firebase
 import FirebaseDatabase
 
-class MyPageViewController: UITableViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
-
-    @IBOutlet weak var userPicture: UIImageView!
-    @IBOutlet weak var userNickName: UILabel!
-    @IBOutlet weak var userIntro: UILabel!
+class MyPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
     var ref: DatabaseReference! = Database.database().reference()
-    
     var userDataModel: FirebaseUserDataModel!
+    // image - db
+    var images = [#imageLiteral(resourceName: "mary"), #imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"), #imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"), #imageLiteral(resourceName: "ddog"), #imageLiteral(resourceName: "mary"), #imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"), #imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"), #imageLiteral(resourceName: "ddog")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
-        
         // 기기 토큰 확인하기
         let deviceToken = UserDefaults.standard.string(forKey: "token")!
         print("마이페이지 기기 토큰 확인:"+deviceToken)
@@ -48,51 +45,30 @@ class MyPageViewController: UITableViewController,UICollectionViewDelegate,UICol
 //            print(error.localizedDescription)
 //        }
     }
-//    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-//        return CGFloat.leastNonzeroMagnitude
-//    }
-//    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-//            return CGFloat.leastNonzeroMagnitude
-//    }
-    
-    // image - db
-    var images = [#imageLiteral(resourceName: "mary"), #imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"),#imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"), #imageLiteral(resourceName: "ddog"), #imageLiteral(resourceName: "mary"), #imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"),#imageLiteral(resourceName: "hana"), #imageLiteral(resourceName: "dog"), #imageLiteral(resourceName: "dog (1)"), #imageLiteral(resourceName: "ddog")]
-    
-    // collectionview 설정 - 가로3장
+    // collectionview 설정 - 이미지 개수 count
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
-    
+    // 이미지 셀 추가
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RowCell", for: indexPath) as! CustomCell
         cell.imageView.image = images[indexPath.row]
         return cell
     }
-    
+    // view size - 가로 3분할, 가로세로같게
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width / 3
+        let width = collectionView.frame.width / 3 - 1
         return CGSize(width: width, height: width)
     }
-    
+    // 위아래 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 1
     }
+    // 양옆 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 1
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 class CustomCell: UICollectionViewCell{
