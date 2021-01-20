fbe9a190c06ff67c791700f4617fd1bddfa80b06
Eun Young <eyeyl0811@naver.com>
2021-01-20 18:53:27 +0900

    delete data model

--- MyPetDiary/DogWalker_iosapp/ViewController/Feed/AddPostViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/Feed/AddPostViewController.swift
@@ -28,7 +28,6 @@ class AddPostViewController: UIViewController, UITextFieldDelegate {
     let picker = UIImagePickerController()
     
     @IBOutlet weak var testView: UIImageView!
-    var ref: DatabaseReference! = Database.database().reference()
     
     // DONE 버튼 눌렀을 경우
     @IBAction func btnSave(_ sender: Any) {
@@ -40,12 +39,12 @@ class AddPostViewController: UIViewController, UITextFieldDelegate {
         let formatter = DateFormatter()
         //formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        var current_date_string = formatter.string(from: Date())
+        let current_date_string = formatter.string(from: Date())
         
         // 날짜 데이터 없을 경우 처리
         if receivedPostDate == "" {
             formatter.dateFormat = "yyyy-MM-dd"
-            var default_post_date = formatter.string(from: Date())
+            let default_post_date = formatter.string(from: Date())
             receivedPostDate = default_post_date
         }
         
@@ -67,33 +66,29 @@ class AddPostViewController: UIViewController, UITextFieldDelegate {
                     receivedImageURL = info as! String
                 }
                 dismiss(animated: true, completion: nil)
-                
-                
             }
             
             // copy text for DB
             contentToDB = textField.text!
             
-            print("copy Text 확인용:"+contentToDB)
-            
-            let postRef = self.ref.child("Post")
-        
-            let DateRefKey = postRef.child("\(deviceToken)").child("\(receivedPostDate)")
-            
-            let post = ["post_content": contentToDB, // 글 내용 저장
-                        "post_updated_date": current_date_string, // 글 업로드 시기 저장
-                        "post_date": receivedPostDate, // 글 자체의 날짜 저장
-                        "post_walk": receivedWalkSwitch, // 산책, 목욕, 약, 병원, 스위치 상태 저장
-                        "post_wash": receivedWashSwitch,
-                        "post_medicine": receivedMedicineSwitch,
-                        "post_image": receivedImageURL,
-                        "post_hospital": receivedHospitalSwitch] as [String : Any]
-            
-            DateRefKey.setValue(post)
-            
-            //
+            uploadToDB(deviceToken: deviceToken, current_date_string: current_date_string)
         }
         
+    }
+    
+    func uploadToDB(deviceToken: String, current_date_string: String) {
+        let postRef: DatabaseReference! = Database.database().reference().child("Post").child("\(deviceToken)").child("\(receivedPostDate)")
+        
+        let post = ["post_content": contentToDB, // 글 내용 저장
+                    "post_updated_date": current_date_string, // 글 업로드 시기 저장
+                    "post_date": receivedPostDate, // 글 자체의 날짜 저장
+                    "post_walk": receivedWalkSwitch, // 산책, 목욕, 약, 병원, 스위치 상태 저장
+                    "post_wash": receivedWashSwitch,
+                    "post_medicine": receivedMedicineSwitch,
+                    "post_image": receivedImageURL,
+                    "post_hospital": receivedHospitalSwitch] as [String : Any]
+        
+        postRef.setValue(post)
     }
     
     override func viewDidLoad() {
@@ -107,22 +102,18 @@ class AddPostViewController: UIViewController, UITextFieldDelegate {
         print("약 스위치 확인: \(receivedMedicineSwitch)")
         print("병원 스위치 확인: \(receivedHospitalSwitch)")
         
-        
         // 기기 토큰 확인하기
         let deviceToken = UserDefaults.standard.string(forKey: "token")!
         print("글 쓰기 기기 토큰 확인:"+deviceToken)
         
-//        ref.child("Post").child("\(deviceToken)").observeSingleEvent(of: .value, with: {(snapshot) in
-//            for child in snapshot.children {
-//                let snap = child as! DataSnapshot
-//                if dictionary["post_date"] as! String == showDate {
-//                    print(snap.key) // showDate가 들어있는 상위 디렉토리 이름
-//                    print(dictionary["post_content"] as? String)
-//                }
-//            }
-//        })
+        checkDB(deviceToken: deviceToken)
+    }
+    
+    // 기존에 쓴 데이터가 있는지 확인하기
+    func checkDB(deviceToken: String) {
+        let ref: DatabaseReference! = Database.database().reference().child("Post").child("\(deviceToken)")
         
-        ref.child("Post").child("\(deviceToken)").observeSingleEvent(of: .value, with: {(snapshot) in
+        ref.observeSingleEvent(of: .value, with: {(snapshot) in
             if snapshot.exists() {
                 let values = snapshot.value
                 let dic = values as! [String : [String:Any]]
