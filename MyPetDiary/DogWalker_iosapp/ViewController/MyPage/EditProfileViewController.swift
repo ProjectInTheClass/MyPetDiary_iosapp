b37d6671a862ee3eee3483302a707ff9b32774a2
jiyeon <jjy1000@dgu.ac.kr>
2021-01-21 15:25:39 +0900

    mypage 계속 수정중

---
+++ MyPetDiary/DogWalker_iosapp/ViewController/MyPage/EditProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EditProfileViewController.swift
+//  DogWalker_iosapp
+//
+//  Created by 정지연 on 2021/01/21.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var editImageButton: UIButton!
+    @IBOutlet weak var editIDTextField: UITextField!
+    @IBOutlet weak var editIntroTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
