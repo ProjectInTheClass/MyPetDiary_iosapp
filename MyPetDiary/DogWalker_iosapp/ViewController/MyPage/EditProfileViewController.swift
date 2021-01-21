8f9300b915dfc8f017d58b20cd388ddf894d8e86
jiyeon <jjy1000@dgu.ac.kr>
2021-01-21 15:28:10 +0900

    Update EditProfileViewController.swift

--- MyPetDiary/DogWalker_iosapp/ViewController/MyPage/EditProfileViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/MyPage/EditProfileViewController.swift
@@ -28,33 +28,33 @@ class EditProfileViewController: UIViewController {
 
 }
 
-extension EditProfileViewController : UIImagePickerControllerDelegate,
-UINavigationControllerDelegate{
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-        {
-            userImage.image = image
-            print("testImage\(image)")
-        }
-        let imageUrl=info[UIImagePickerController.InfoKey.imageURL] as? NSURL
-        let imageName=imageUrl?.lastPathComponent//파일이름
-        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-                     
-        let photoURL = NSURL(fileURLWithPath: documentDirectory)
-        print("photoURL\(photoURL)")
-        let localPath = photoURL.appendingPathComponent(imageName!)//파일경로
-        let data=NSData(contentsOf: imageUrl as! URL)!
-        print("lastURL:\(localPath!.path)")
-        //localFile = String(describing: localPath)
-        var str = "file:///"
-        let fileURL = "\(localPath!.path)"
-        str.append(fileURL)
-        localFile = str
-        //localFile = String(decoding: localPath!, as: UTF8.self)
-        print("localFile:"+localFile)
-        dismiss(animated: true, completion: nil)
-      
-    }
-}
+//extension EditProfileViewController : UIImagePickerControllerDelegate,
+//UINavigationControllerDelegate{
+//
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+//        {
+//            userImage.image = image
+//            print("testImage\(image)")
+//        }
+//        let imageUrl=info[UIImagePickerController.InfoKey.imageURL] as? NSURL
+//        let imageName=imageUrl?.lastPathComponent//파일이름
+//        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+//
+//        let photoURL = NSURL(fileURLWithPath: documentDirectory)
+//        print("photoURL\(photoURL)")
+//        let localPath = photoURL.appendingPathComponent(imageName!)//파일경로
+//        let data=NSData(contentsOf: imageUrl as! URL)!
+//        print("lastURL:\(localPath!.path)")
+//        //localFile = String(describing: localPath)
+//        var str = "file:///"
+//        let fileURL = "\(localPath!.path)"
+//        str.append(fileURL)
+//        localFile = str
+//        //localFile = String(decoding: localPath!, as: UTF8.self)
+//        print("localFile:"+localFile)
+//        dismiss(animated: true, completion: nil)
+//
+//    }
+//}
