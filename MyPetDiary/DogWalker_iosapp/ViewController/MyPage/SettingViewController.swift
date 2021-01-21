3104058bff783c1908965358d7a0008c9b147889
jiyeon <jjy1000@dgu.ac.kr>
2021-01-21 20:15:40 +0900

    #19

--- MyPetDiary/DogWalker_iosapp/ViewController/MyPage/SettingViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/MyPage/SettingViewController.swift
@@ -6,49 +6,18 @@
 //
 
 import UIKit
-import MessageUI
 
-class SettingViewController: UITableViewController, MFMailComposeViewControllerDelegate {
+class SettingViewController: UITableViewController {
 
     @IBOutlet var settingTableView: UITableView!
     @IBAction func unwindFromModal (seque: UIStoryboardSegue) {}
     @IBAction func unwindFromEditProfile(seque: UIStoryboardSegue){}
     
-    @IBOutlet weak var sendEmailCell: UITableViewCell!
-
-    let composeVC = MFMailComposeViewController()
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           print("section: \(indexPath.section)")
-           print("row: \(indexPath.row)")
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    func sendEmail() {
-        composeVC.mailComposeDelegate = self
-        // Configure the fields of the interface.
-        composeVC.setToRecipients(["jjy1000@dgu.ac.kr"])
-        composeVC.setSubject("Hello!")
-        composeVC.setMessageBody("의견을 보내주세요!", isHTML: false)
-        // Present the view controller modally.
-        self.present(composeVC, animated: true, completion: nil)
-    }
-
-    private func mailComposeController(controller: MFMailComposeViewController,
-                           didFinishWithResult result: MFMailComposeResult, error: NSError?) {
-        // Check the result or perform other tasks.
-        // Dismiss the mail compose view controller.
-        controller.dismiss(animated: true, completion: nil)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-//
+    
 //    func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
 //        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
 //        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
@@ -62,15 +31,4 @@ class SettingViewController: UITableViewController, MFMailComposeViewControllerD
 //        self.view.addSubview(toastLabel)
 //        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: { toastLabel.alpha = 0.0 }, completion: {(isCompleted) in toastLabel.removeFromSuperview() })
 //    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
