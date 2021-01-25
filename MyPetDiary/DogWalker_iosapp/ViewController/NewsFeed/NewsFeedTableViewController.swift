02e78046a683e7bdb9db2a8a936e5d253a01be1f
jiyeon <jjy1000@dgu.ac.kr>
2021-01-25 20:10:49 +0900

    피드내용

--- MyPetDiary/DogWalker_iosapp/ViewController/NewsFeed/NewsFeedTableViewController.swift
+++ MyPetDiary/DogWalker_iosapp/ViewController/NewsFeed/NewsFeedTableViewController.swift
@@ -11,6 +11,7 @@ class NewsFeedTableViewController: UITableViewController {
     
     var posts: [Post]?
 
+    @IBOutlet weak var contentTextView: UITextView!
     func initRefresh(){
         let refresh = UIRefreshControl()
         tableView.refreshControl = refresh
